504a8836abbd63ea5c2d090c8b5c953f06ea14c7
Shwetansh Welfare and Educational Trust <shwetanshwelfareeducational@gmail.com>
2026-03-07 03:15:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'swet_ai_labs',
+          dest: ''
+        }
+      ]
+    })
+  ],
   server: {
     allowedHosts: [
       '' // for the local developement testing using cloudflared tunnels
